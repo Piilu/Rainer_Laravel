806c267627c84d48a0ab4a380946ab9e9a56cd43
Piilu <piilrainer@gmail.com>
2022-06-09 11:52:03 +0300

    Admini View nupp ja postitused profiilil

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>Laraveli töö</h1>
+
+1. Laravelile lisada view or show page adminile. (Nüüd kui admin lehelt vajutada "View", siis avatakse postitus)
+1. Laravelile lisada kasutaja vaatele ainult tema postitused. (Nüüd kui minna kasutaja profiilile on näha ka selle kasutaja postitusi)
---
+++ app/Http/Controllers/PublicController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class PublicController extends Controller
+{
+    public function home(){
+        return view('welcome');
+    }
+
+    public function posts(){
+        $posts = Post::published()->latest()->paginate(12);
+
+//        dd($posts->toArray());
+        return view('posts', compact('posts'));
+    }
+    public function tag($tag){
+        $tag = Tag::where('name', $tag)->firstOrFail();
+        $posts = $tag->posts()->whereNotNull('published_at')->latest()->paginate(12);
+        return view('posts', compact('posts'));
+    }
+
+    public function post(Post $post){
+        return view('post', compact('post'));
+    }
+    public function user(User $user){
+        $posts = Post::where("user_id",$user->id)->paginate(12);
+
+        return view('user', compact(['posts',"user"]));
+    }
+}
